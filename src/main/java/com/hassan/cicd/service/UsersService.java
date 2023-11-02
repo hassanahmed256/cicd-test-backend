9d1fdd6af1df8f0b841c57e383e17908afe9529a
hassanahmed256 <hassan765***>
2023-11-02 12:04:36 +0500

    commit

--- src/main/java/com/hassan/cicd/service/UsersService.java
+++ src/main/java/com/hassan/cicd/service/UsersService.java
@@ -1,19 +1,19 @@
-package com.hassan.cicd.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.hassan.cicd.models.Users;
-import com.hassan.cicd.repository.UsersRepository;
-
-@Service
-public class UsersService {
-	@Autowired
-	UsersRepository usersRepository;
-
-	public List<Users> getAllUsers(){
-		return usersRepository.findAll();
-	}
-}
+//package com.hassan.cicd.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.hassan.cicd.models.Users;
+//import com.hassan.cicd.repository.UsersRepository;
+//
+//@Service
+//public class UsersService {
+//	@Autowired
+//	UsersRepository usersRepository;
+//
+//	public List<Users> getAllUsers(){
+//		return usersRepository.findAll();
+//	}
+//}
